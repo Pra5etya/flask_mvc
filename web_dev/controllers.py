a5631498c472da020b7759d72af3e037eb5e8955
Pra5etya <careermagic780@gmail.com>
2024-06-10 15:22:45 +0700

    mod

--- web_dev/controllers.py
+++ web_dev/controllers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:696fb10677d6d919cf9734f6339eef20708182d90c77f6ce7b1294aecee4ac37
-size 897
+oid sha256:b6a7c81980e94c34a53568a5b5cfd6fd054e4c68485ab9090d8f51db3f81b1a2
+size 938
--- web_dev/views.py
+++ web_dev/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed34757b844ba230d1da7abf3a12bb81108d4cb6b3c1279daa9573bf872d3614
-size 1665
+oid sha256:946dd293739229e962d8c130a202369d7ec9dd9e9832dd472f18c730900a1514
+size 1452
